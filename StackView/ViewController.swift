41a79413deccf5159340a6690ec5083fae66ba20
Kuma <Kuma@Macs-MacBook-Pro.local>
2017-04-29 15:00:51 -0500

    finished the try

---
+++ StackView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StackView
+//
+//  Created by Kuma on 4/29/17.
+//  Copyright © 2017 Kuma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
